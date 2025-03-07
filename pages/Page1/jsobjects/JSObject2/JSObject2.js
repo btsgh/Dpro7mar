8ce73592cf832fc6677b1d6d539ba8cfe9d061b7
Aparna Ramachandran <aparna@appsmith.com>
2025-03-07 04:07:17 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/JSObject2/JSObject2.js
@@ -0,0 +1,14 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		console.log(_.add( 678214 + 79431) )	
+		
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
